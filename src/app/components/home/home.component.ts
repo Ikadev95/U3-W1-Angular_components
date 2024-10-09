3b152510abc15e3e353c30705a889576fce96938
Ikadev95 <fegughi@gmail.com>
2024-10-09 16:47:39 +0200

    post ok!

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Post} from '../post.js'
 
 @Component({
   selector: 'app-home',
@@ -6,12 +7,26 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  post: Post[] = [];
+  titleHeader: string = "";
+  bodyHeader: string = "";
+  tagsHeader: Array<string> = []
   ngOnInit(){ // caricamento del componente
     fetch('./assets/db.json')
     .then((res)=>res.json())
     .then((dati)=>{
-      console.log(dati)
+      console.log(dati.posts);
+      for ( let i = 1 ; i < dati.posts.length; i++){
+        this.post.push(dati.posts[i]);
+        let singlePost: Post = dati.posts[i];
+
+      }
+      console.log(this.post[0].title)
+      this.titleHeader = this.post[0].title
+      this.bodyHeader = this.post[0].body
+      this.tagsHeader = this.post[0].tags
     })
     .catch((err)=>{console.log(err)})
   }
 }
+
