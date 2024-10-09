0e5d3fbbc96c848497304bfb5f0a316c3d284f1a
Ikadev95 <fegughi@gmail.com>
2024-10-09 16:57:37 +0200

    clean

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -15,16 +15,16 @@ export class HomeComponent {
     fetch('./assets/db.json')
     .then((res)=>res.json())
     .then((dati)=>{
-      console.log(dati.posts);
+      //console.log(dati.posts);
+      this.titleHeader = dati.posts[0].title
+      this.bodyHeader = dati.posts[0].body
+      this.tagsHeader = dati.posts[0].tags
+
       for ( let i = 1 ; i < dati.posts.length; i++){
         this.post.push(dati.posts[i]);
-        let singlePost: Post = dati.posts[i];
-
       }
-      console.log(this.post[0].title)
-      this.titleHeader = this.post[0].title
-      this.bodyHeader = this.post[0].body
-      this.tagsHeader = this.post[0].tags
+     // console.log(this.post[0].title)
+
     })
     .catch((err)=>{console.log(err)})
   }
