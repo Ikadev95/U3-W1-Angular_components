71693e286c6d51fb25a443ea3a70494c7659846b
Ikadev95 <fegughi@gmail.com>
2024-10-10 14:40:09 +0200

    giorno2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { InactivePostsComponent } from './components/inactive-posts/inactive-posts.component';
+import { ActivePostsComponent } from './components/active-posts/active-posts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'inactive-post',
+    component: InactivePostsComponent,
+  },
+  {
+    path: 'active-post',
+    component: ActivePostsComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
